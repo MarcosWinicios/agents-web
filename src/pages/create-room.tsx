cc04d022385a4ba07664d58b28e709a63067072c
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-07-13 05:58:53 -0300

    feat(room): Add room listing API consumption

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,9 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 
+type GetRoomsApiResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms');
+      const result: GetRoomsApiResponse = await response.json();
+
+      return result;
+    },
+  });
+
   return (
     <div>
-      <div> Create Room</div>
+      {isLoading && <p> Carregando...</p>}
+
+      <div>
+        {data?.map((room) => {
+          return <p key={room.id}>{room.name}</p>;
+        })}
+      </div>
+
       <Link className="underline" to="/room">
         Acessar sala
       </Link>
