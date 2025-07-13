f219f61045c9da86531c687fee370a746bd6f786
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-07-13 05:40:45 -0300

    feat(route): Add router between pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from './pages/create-room';
+import { Room } from './pages/room';
 
 export function App() {
-  return <div />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom />} index />
+        <Route element={<Room />} path="/room" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
