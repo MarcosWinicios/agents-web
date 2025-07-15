f4eb8e3aaa6e57040595fcc209890796b7077f84
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-07-15 01:50:28 -0300

    feat(audio): Add audio recorder api consumption

--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -1,5 +1,6 @@
 /** biome-ignore-all lint/suspicious/noConsole: <explanation> */
 import { useRef, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 
 const isRecordingSupported =
@@ -7,7 +8,12 @@ const isRecordingSupported =
   typeof navigator.mediaDevices.getUserMedia === 'function' &&
   typeof window.MediaRecorder === 'function';
 
+type RoomParams = {
+  roomId: string;
+};
+
 export function RecordRoomAudio() {
+  const params = useParams<RoomParams>();
   const [isRecording, setIsRecording] = useState(false);
   const recorder = useRef<MediaRecorder | null>(null);
 
@@ -17,6 +23,24 @@ export function RecordRoomAudio() {
     if (recorder.current && recorder.current.state !== 'inactive') {
       recorder.current.stop();
     }
+  }
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+
+    formData.append('file', audio, 'audio.webm');
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: 'POST',
+        body: formData,
+      }
+    );
+
+    const result = await response.json();
+
+    console.log(result);
   }
 
   async function startRecording() {
@@ -41,7 +65,7 @@ export function RecordRoomAudio() {
 
     recorder.current.ondataavailable = (event) => {
       if (event.data.size > 0) {
-        console.log(event.data);
+        uploadAudio(event.data);
       }
     };
 
@@ -56,6 +80,9 @@ export function RecordRoomAudio() {
     recorder.current.start();
   }
 
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-3">
       {isRecording ? (
